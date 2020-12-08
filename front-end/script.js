2f4e4cf8ae9c08c4a9d3a094283f4439e1d7c763
sunflail <sunflail2001@yahoo.com>
2020-12-08 15:27:49 -0800

    added simple front end

---
+++ front-end/script.js
@@ -0,0 +1,39 @@
+const url = "https://sunflail-crud-demo.herokuapp.com/employees";
+
+// get the input information when the button is clicked
+const myForm = document.querySelector("#myForm");
+myForm.onsubmit = (event) => {
+  event.preventDefault();
+
+  let fName = event.target.firstName.value;
+  let lName = event.target.lastName.value;
+  let email = event.target.email.value;
+  let role = event.target.role.value;
+
+  console.log({ fName, lName, email, role });
+  let userObj = { fName, lName, email, role };
+
+  // pass the info via CRUD to the back end server
+  // send this object via POST to https://sunflail-crud-demo.herokuapp.com/employees
+
+  axios.post(url, userObj).then((res) => {
+    console.log(res.data);
+  });
+
+  event.target.reset();
+};
+
+const showAllBtn = document.querySelector("#showAllBtn");
+showAllBtn.onclick = (event) => {
+  showAllEmpInfo();
+};
+
+function showAllEmpInfo(event) {
+  let allEmpsInfo;
+  axios.get(url).then((res) => {
+    allEmpsInfo = res.data;
+    let dataP = document.createElement("p");
+    dataP.innerHTML = JSON.stringify(allEmpsInfo);
+    document.querySelector("#dataContainer").appendChild(dataP);
+  });
+}
